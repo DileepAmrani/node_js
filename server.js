848935000abc30760222c268bdacb6919b03adcd
DileepAmrani <amranidileep44@gmail.com>
2022-09-17 10:53:30 +0500

    complete crud mern

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+// mongodb+srv://amranidileep44:amrani786@amranicluster.ixjmrsm.mongodb.net/mearnapp
+
+connectDB();
 
 const app = express();
 app.use(express.json());
